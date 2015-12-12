d727a8a571d231f56de6cb8b8f56ca4c50a176f6
Trent B. Bingham <manachar@gmail.com>
2015-12-12 12:35:57 -0500

    Add looping auto advancing functionality

--- x/0003/js/main.js
+++ x/0003/js/main.js
@@ -1,7 +1,39 @@
 $(function() {
-  var now = new Date();
-  var $cal = $('.cal'),
-      $days = $('.cal-day', $cal);
+  var now     = new Date(),
+      $cal    = $('.cal'),
+      $days   = $('.cal-day', $cal),
+      $audios = $('audio'),
+      okDays  = [];
+
+  var playAudio = function($day) {
+
+    var $control = $('.fa', $day),
+        day      = $day.data('day'),
+        $audio   = $('#audio-xmas-2015-' + day),
+        status   = $day.data('status');
+
+    if ( status !== 'playing') {
+      $audio[0].play();
+      $control.removeClass('fa-play').addClass('fa-pause');
+      $day.data('status', 'playing');
+    }
+
+  };
+
+  var pauseAudio = function($day) {
+
+    var $control = $('.fa', $day),
+        day      = $day.data('day'),
+        $audio   = $('#audio-xmas-2015-' + day),
+        status   = $day.data('status');
+
+    if ( status !== 'paused') {
+      $audio[0].pause();
+      $control.removeClass('fa-pause').addClass('fa-play');
+      $day.data('status', 'paused');
+    }
+
+  };
 
   $days.each(function(){
     var $day = $(this),
@@ -11,6 +43,7 @@ $(function() {
         date = new Date(year, month, day);
     if (date && now >= date){
       $day.addClass('ok');
+      okDays.push(day);
     }
   });
 
@@ -22,20 +55,30 @@ $(function() {
 
   $cal.on('click', '.cal-day.active', function(event){
     var $day = $(this),
-        $control = $('.fa', $day),
-        day = $day.data('day'),
-        $audio = $('#audio-xmas-2015-' + day),
         status = $day.data('status');
     if(status=='playing'){
-      $audio[0].pause();
-      $control.removeClass('fa-pause').addClass('fa-play');
-      $day.data('status', 'paused');
+      pauseAudio($day);
     }
     else {
-      $audio[0].play();
-      $control.removeClass('fa-play').addClass('fa-pause');
-      $day.data('status', 'playing');
+      playAudio($day);
     }
+  });
+
+  $audios.on('ended', function(event){
+    var $this = $(this),
+        day = $this.data('day'),
+        nextDay = day + 1;
+
+    var $day        = $('.cal-day[data-day="' + day + '"]'),
+        $dayControl = $('.fa', $day),
+        $nextDay    = $('.cal-day[data-day="' + ( okDays.indexOf(nextDay) !== -1 ? nextDay : 1 ) + '"]');
+
+    $day.data('status', null);
+    $day.removeClass('active');
+    $dayControl.removeClass('fa-pause').addClass('fa-play');
+    $nextDay.addClass('active');
+    playAudio($nextDay);
+
   });
 
 });
